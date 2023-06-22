ee20082ec24b677b56033c490dec3cab6f753d31
maof97 <48106483+maof97@users.noreply.github.com>
2023-06-23 00:03:06 +0200

    Better logging

--- go.mod
+++ go.mod
@@ -4,4 +4,8 @@ go 1.20
 
 require github.com/elastic/go-elasticsearch/v8 v8.8.1
 
-require github.com/elastic/elastic-transport-go/v8 v8.3.0 // indirect
+require (
+	github.com/elastic/elastic-transport-go/v8 v8.3.0 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+)
--- server.go
+++ server.go
@@ -4,26 +4,34 @@ import (
 	"context"
 	"crypto/tls"
 	"crypto/x509"
+	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
+	"log/syslog"
 	"net/http"
 	"os"
 	"strings"
 	"time"
 
 	"github.com/elastic/go-elasticsearch/v8"
+	"github.com/sirupsen/logrus"
+	logrus_syslog "github.com/sirupsen/logrus/hooks/syslog"
 )
 
 const (
-	CERT_FILE             = "/path/to/server.crt"
-	KEY_FILE              = "/path/to/server.key"
-	CA_FILE               = "/path/to/ca.crt"
-	ES_URL                = "http://localhost:9200"
+	SERVER_URL            = "localhost:8080"
+	CERT_FILE             = "certs/server.crt"
+	KEY_FILE              = "certs/server.key"
+	CA_FILE               = "certs/ca.crt"
+	ES_URL                = "https://10.20.1.6:9200"
 	INDEX_NAME            = "pfelk-*"
-	ES_USERNAME           = "elastic"
+	ES_USERNAME           = "zsoar"
 	ES_PASSWORD_ENV       = "ES_PASSWORD"
+	ES_IGNORE_CERT_ERRORS = true
 	LOOKBACK_TIME_MINUTES = 90
+	LOG_SYSLOG_SERVER     = "10.20.1.1:514" // Leave empty to disable
 )
 
 func main() {
@@ -55,24 +63,76 @@ func main() {
 		Addresses: []string{ES_URL},
 		Username:  ES_USERNAME,
 		Password:  os.Getenv(ES_PASSWORD_ENV), // retrieve password from environment variable
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify: ES_IGNORE_CERT_ERRORS,
+			},
+		},
 	}
 	es, err := elasticsearch.NewClient(cfg)
 	if err != nil {
 		log.Fatalf("error setting up Elasticsearch client: %s", err)
 	}
 
+	// Set up logrus logger
+	logger := logrus.New()
+	logger.SetFormatter(&logrus.JSONFormatter{
+		TimestampFormat: time.RFC3339,
+	})
+	logFile, err := os.OpenFile("server.log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		log.Fatalf("failed to open log file: %v", err)
+	}
+	defer logFile.Close()
+
+	// Set up syslog writer if needed
+	if LOG_SYSLOG_SERVER != "" {
+		hook, err := logrus_syslog.NewSyslogHook("udp", LOG_SYSLOG_SERVER, syslog.LOG_INFO, "")
+		if err != nil {
+			logger.Error("Unable to connect to local syslog daemon")
+		} else {
+			logger.AddHook(hook)
+		}
+	}
+	mw := io.MultiWriter(os.Stdout, logFile)
+	logrus.SetOutput(mw)
+
 	// Set up HTTP server
 	server := &http.Server{
-		Addr:         ":8080",
+		Addr:         SERVER_URL,
 		TLSConfig:    tlsConfig,
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 10 * time.Second,
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			// Check if request is a GET request
+			if r.Method != http.MethodGet {
+				http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+				logger.WithFields(logrus.Fields{
+					"ip":     r.RemoteAddr,
+					"method": r.Method,
+				}).Warning("Client requested invalid method")
+				return
+			}
+
+			// Check if path is valid
+			if !strings.HasPrefix(r.URL.Path, "/interface_check/") {
+				http.Error(w, "Invalid request path", http.StatusNotFound)
+				logger.WithFields(logrus.Fields{
+					"ip":   r.RemoteAddr,
+					"path": r.URL.Path,
+				}).Warning("Client requested invalid path")
+				return
+			}
+
 			// Extract interface from request path
 			interfaceName := strings.TrimPrefix(r.URL.Path, "/interface_check/")
 			// Sanitize the input to prevent path traversal attacks
 			if strings.Contains(interfaceName, "/") {
 				http.Error(w, "Invalid interface name", http.StatusBadRequest)
+				logger.WithFields(logrus.Fields{
+					"ip":        r.RemoteAddr,
+					"interface": interfaceName,
+				}).Warning("Client requested invalid interface name")
 				return
 			}
 			// TODO: Check if interface is available
@@ -112,10 +172,49 @@ func main() {
 			fmt.Println(res.String())
 
 			if err != nil {
-				log.Printf("error performing search: %s", err)
 				http.Error(w, "Error performing search", http.StatusInternalServerError)
+				logger.WithFields(logrus.Fields{
+					"ip":    r.RemoteAddr,
+					"error": err,
+				}).Error("Error performing Eleasticsearch search")
 				return
 			}
+
+			if res.IsError() {
+				// Log error message
+				logger.WithFields(logrus.Fields{
+					"ip":              r.RemoteAddr,
+					"response_status": res.Status(),
+					"response_string": res.String(),
+				}).Error("Error performing Eleasticsearch search")
+				return
+			}
+
+			defer res.Body.Close()
+
+			body, err := ioutil.ReadAll(res.Body)
+			if err != nil {
+				logger.WithFields(logrus.Fields{
+					"ip":    r.RemoteAddr,
+					"error": err,
+				}).Error("Error reading response body")
+				http.Error(w, "Error reading response body from ES", http.StatusInternalServerError)
+				return
+			}
+
+			var data map[string]interface{}
+			err = json.Unmarshal(body, &data)
+			if err != nil {
+				logger.WithFields(logrus.Fields{
+					"ip":    r.RemoteAddr,
+					"error": err,
+				}).Error("Error unmarshalling response body")
+				http.Error(w, "Error unmarshalling response body from ES", http.StatusInternalServerError)
+				return
+			}
+
+			// Print the data to stdout
+			fmt.Println(data)
 
 			/* if res.IsError() {
 				log.Printf("search request failed: %s", res.Status())
